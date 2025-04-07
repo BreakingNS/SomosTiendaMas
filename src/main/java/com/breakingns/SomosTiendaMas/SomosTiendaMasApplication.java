fa5844275487720fd9d8c7bccd9b74e5edaf4a13
BreakingNS <nahuel_segura_17@hotmail.com>
2025-04-07 19:12:16 -0300

    creacion del proyecto SomosTiendaMas

---
+++ src/main/java/com/breakingns/SomosTiendaMas/SomosTiendaMasApplication.java
@@ -0,0 +1,13 @@
+package com.breakingns.SomosTiendaMas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomosTiendaMasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomosTiendaMasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breakingns/SomosTiendaMas/SomosTiendaMasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breakingns.SomosTiendaMas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomosTiendaMasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
